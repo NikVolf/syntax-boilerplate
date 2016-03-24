695946b48e84e0c8a2b3910c042445392faff005
NikVolf <nikvolf@gmail.com>
2016-03-24 19:50:16 +0300

    working dispatch with arbitrary parameters

--- src/codegen.rs
+++ src/codegen.rs
@@ -62,28 +62,31 @@ fn push_invoke_signature_aster (
     push: &mut FnMut(Annotatable),
 ) -> Dispatch {
 
-	let mut dispath = Dispatch { input_type_name: None, return_type_name: None };
-
 	let inputs = &signature.decl.inputs;
-	let input_type_name = if inputs.len() > 0 {
+	let (input_type_name, input_arg_names) = if inputs.len() > 0 {
 		let first_field_name = field_name(builder, &inputs[0]).name.as_str();
-		if  first_field_name == "self" && inputs.len() == 1 { None }
+		if first_field_name == "self" && inputs.len() == 1 { (None, vec![]) }
 		else {
 			let skip = if first_field_name == "self" { 2 } else { 1 };
 			let name_str = format!("{}_input", implement.ident.name.as_str());
 
+			let mut arg_names = Vec::new();
+			let arg_name = format!("{}", field_name(builder, &inputs[skip-1]).name);
 			let mut tree = builder.item().attr().word("derive(Serialize, Deserialize)").struct_(name_str.as_str())
-				.field(format!("{}", field_name(builder, &inputs[skip-1]).name.as_str	())).ty().build(inputs[skip-1].ty.clone());
+				.field(arg_name.as_str()).ty().build(inputs[skip-1].ty.clone());
+			arg_names.push(arg_name);
 			for arg in inputs.iter().skip(skip) {
-				tree = tree.field(format!("{}", field_name(builder, &arg).name.as_str())).ty().build(arg.ty.clone());
+				let arg_name = format!("{}", field_name(builder, &arg));
+				tree = tree.field(arg_name.as_str()).ty().build(arg.ty.clone());
+				arg_names.push(arg_name);
 			}
 
 			push(Annotatable::Item(tree.build()));
-			Some(name_str.to_owned())
+			(Some(name_str.to_owned()), arg_names)
 		}
 	}
 	else {
-		None
+		(None, vec![])
 	};
 
 	let return_type_name = match signature.decl.output {
@@ -98,14 +101,98 @@ fn push_invoke_signature_aster (
 	};
 
 	Dispatch {
+		function_name: format!("{}", implement.ident.name.as_str()),
 		input_type_name: input_type_name,
+		input_arg_names: input_arg_names,
 		return_type_name: return_type_name,
 	}
 }
 
 struct Dispatch {
+	function_name: String,
 	input_type_name: Option<String>,
+	input_arg_names: Vec<String>,
 	return_type_name: Option<String>,
+}
+
+fn implement_dispatch_arm(
+	cx: &ExtCtxt,
+    builder: &aster::AstBuilder,
+	dispatch: &Dispatch,
+) -> P<ast::Expr>
+{
+	let deserialize_expr = quote_expr!(cx, ::bincode::serde::deserialize_from(r, ::bincode::SizeLimit::Infinite).expect("ipc deserialization error, aborting"));
+	let input_type_id = builder.id(dispatch.input_type_name.clone().unwrap().as_str());
+	let output_type_id = builder.id(dispatch.return_type_name.clone().unwrap().as_str());
+	let function_name = builder.id(dispatch.function_name.as_str());
+
+	let input_args_exprs = dispatch.input_arg_names.iter().map(|ref arg_name| {
+		let arg_ident = builder.id(arg_name);
+		quote_expr!(cx, input. $arg_ident)
+	}).collect::<Vec<P<ast::Expr>>>();
+
+
+	//	This is the expanded version of this:
+	//
+	//	let invoke_serialize_stmt = quote_stmt!(cx, {
+	//		::bincode::serde::serialize(& $output_type_id { payload: self. $function_name ($hand_param_a, $hand_param_b) }, ::bincode::SizeLimit::Infinite).unwrap()
+	//	});
+	//
+	//	But the above does not allow comma-separated expressions
+	let invoke_serialize_stmt = {
+		let ext_cx = &*cx;
+		::quasi::parse_stmt_panic(&mut ::syntax::parse::new_parser_from_tts(
+			ext_cx.parse_sess(),
+			ext_cx.cfg(),
+			{
+				let _sp = ext_cx.call_site();
+				let mut tt = ::std::vec::Vec::new();
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::OpenDelim(::syntax::parse::token::Brace)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::ModSep));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Ident(ext_cx.ident_of("bincode"), ::syntax::parse::token::ModName)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::ModSep));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Ident(ext_cx.ident_of("serde"), ::syntax::parse::token::ModName)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::ModSep));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Ident(ext_cx.ident_of("serialize"), ::syntax::parse::token::Plain)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::OpenDelim(::syntax::parse::token::Paren)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::BinOp(::syntax::parse::token::And)));
+				tt.extend(::quasi::ToTokens::to_tokens(&output_type_id, ext_cx).into_iter());
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::OpenDelim(::syntax::parse::token::Brace)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Ident(ext_cx.ident_of("payload"), ::syntax::parse::token::Plain)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Colon));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Ident(ext_cx.ident_of("self"), ::syntax::parse::token::Plain)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Dot));
+				tt.extend(::quasi::ToTokens::to_tokens(&function_name, ext_cx).into_iter());
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::OpenDelim(::syntax::parse::token::Paren)));
+
+				for arg_expr in input_args_exprs {
+					tt.extend(::quasi::ToTokens::to_tokens(&arg_expr, ext_cx).into_iter());
+					tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Comma));
+				}
+
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::CloseDelim(::syntax::parse::token::Paren)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::CloseDelim(::syntax::parse::token::Brace)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Comma));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::ModSep));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Ident(ext_cx.ident_of("bincode"), ::syntax::parse::token::ModName)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::ModSep));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Ident(ext_cx.ident_of("SizeLimit"), ::syntax::parse::token::ModName)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::ModSep));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Ident(ext_cx.ident_of("Infinite"), ::syntax::parse::token::Plain)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::CloseDelim(::syntax::parse::token::Paren)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Dot));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::Ident(ext_cx.ident_of("unwrap"), ::syntax::parse::token::Plain)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::OpenDelim(::syntax::parse::token::Paren)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::CloseDelim(::syntax::parse::token::Paren)));
+				tt.push(::syntax::ast::TokenTree::Token(_sp, ::syntax::parse::token::CloseDelim(::syntax::parse::token::Brace)));
+				tt
+			}))
+	};
+	quote_expr!(cx, {
+		let input: $input_type_id = $deserialize_expr;
+		$invoke_serialize_stmt
+	})
+
 }
 
 fn implement_interface(
@@ -136,11 +223,14 @@ fn implement_interface(
 
     let where_clause = &impl_generics.where_clause;
 
+	let mut dispatch_table = Vec::new();
 	for impl_item in impl_items {
 		if let ImplItemKind::Method(ref signature, ref block) = impl_item.node {
-			push_invoke_signature_aster(cx, builder, item, &impl_item, signature, push);
+			dispatch_table.push(push_invoke_signature_aster(cx, builder, item, &impl_item, signature, push));
 		}
 	}
+
+	let dispatch_expr_1 = implement_dispatch_arm(cx, builder, &dispatch_table[1]);
 
     Ok(quote_item!(cx,
         impl $impl_generics ::codegen::interface::IpcInterface<$ty> for $ty $where_clause {
@@ -155,18 +245,17 @@ fn implement_interface(
 				}
 				match method_num[0] + method_num[1]*256 {
 					0 => {
-						let input: new_input = ::bincode::serde::deserialize_from(r, ::bincode::SizeLimit::Infinite).expect("ipc deserialization error, aborting");
-						::bincode::serde::serialize(&new_output { payload: self.new(input.a, input.b) }, ::bincode::SizeLimit::Infinite).unwrap()
+						$dispatch_expr_1
 					}
 					_ => vec![]
 				}
-            }
-			fn invoke<W>(&self, method_num: u16, params: &Option<Vec<u8>>, w: &mut W) -> Vec<u8>
+			}
+			fn invoke<W>(&self, method_num: u16, payload: &Option<Vec<u8>>, w: &mut W) -> Vec<u8>
 				where W: ::std::io::Write
 			{
 				vec![]
 			}
 
-        }
+		}
     ).unwrap())
 }
